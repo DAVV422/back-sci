9ea0c81fdea588a5f48e9508318cfc3caf319693
DAVV422 <diegoalberto42216@gmail.com>
2024-05-26 01:15:26 -0400

    feat(emergency): create emergency module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { ActionModule } from './action/action.module';
 import { AttendsModule } from './attends/attends.module';
 import { SpecificDatesModule } from './specific-dates/specific-dates.module';
 import { DetailEquipmentModule } from './detail-equipment/detail-equipment.module';
+import { EmergencyModule } from './emergency/emergency.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { DetailEquipmentModule } from './detail-equipment/detail-equipment.modul
     AttendsModule,
     SpecificDatesModule,
     DetailEquipmentModule,
+    EmergencyModule,
   ],
 })
 export class AppModule {}
