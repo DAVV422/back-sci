014884244d17bae50bab180271358c0d20797974
DAVV422 <diegoalberto42216@gmail.com>
2024-05-26 01:05:04 -0400

    feat(action): add actionservice

--- src/action/action.module.ts
+++ src/action/action.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ControllersController } from './controllers/controllers.controller';
-import { ActionController } from './controllers/action/action.controller';
 import { ActionController } from './controllers/action.controller';
 import { ActionService } from './services/action.service';
 
-@Module({
-  controllers: [ControllersController, ActionController],
-  providers: [ActionService]
+@Module({  
+  controllers: [ActionController],
+  providers: [ActionService],
 })
 export class ActionModule {}
