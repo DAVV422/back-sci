23c9f0c4649e74c8a5de8c8195d8d82f52a3fb42
DAVV422 <diegoalberto42216@gmail.com>
2024-06-20 07:02:06 -0400

    feat: resource and equipment

--- src/equipment/equipment.module.ts
+++ src/equipment/equipment.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EquipmentController } from './controllers/equipment.controller';
 import { EquipmentService } from './services/equipment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EquipmentEntity } from './entities/equipment.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([EquipmentEntity]),
+    UserModule
+  ],
   controllers: [EquipmentController],
-  providers: [EquipmentService]
+  providers: [EquipmentService],
+  exports: [EquipmentService]
 })
 export class EquipmentModule {}
--- src/resource/entities/resource.entity.ts
+++ src/resource/entities/resource.entity.ts
@@ -1,1 +1,26 @@
-export class ResourceEntity {}
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { BaseEntity } from '../../common/entities/base.entity';
+import { EmergencyEntity } from '../../emergency/entities/emergency.entity';
+import { EquipmentEntity } from '../../equipment/entities/equipment.entity';
+
+@Entity({ name: 'resource' })
+export class ResourceEntity extends BaseEntity {
+
+  @Column({ name: 'state_initial', type: 'varchar', length: 255, nullable: false })
+  state_initial: string;
+
+  @Column({ name: 'state_end', type: 'varchar', length: 255, nullable: false })
+  state_end: string;
+
+  @Column({ name: 'date', type: 'date', nullable: false })
+  date: Date;
+
+  @Column({ name: 'hour', type: 'varchar', length: 5, nullable: false })
+  hour: string;
+
+  @ManyToOne(() => EmergencyEntity, (emergency) => emergency.resources, { nullable: false, onDelete: 'CASCADE' })
+  emergency: EmergencyEntity;
+
+  @ManyToOne(() => EquipmentEntity, (equipment) => equipment.resources, { nullable: false, onDelete: 'CASCADE' })
+  equipment: EquipmentEntity;
+}
